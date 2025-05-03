d51a53a254fbee75f1872070094e2b5c641b2870
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-05-04 02:12:30 +0300

    task 5 works fine task-6 created.

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const spanText = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+
+changeColorButton.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  spanText.textContent = randomColor;
+});
