9e366c88e34ad88137d38c9ce6abbe4d1ab30ade
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-05-04 00:50:09 +0300

    task 4 works fine without css

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const elements = event.currentTarget.elements;
+  const emailValue = elements.email.value.trim();
+  const passwordValue = elements.password.value.trim();
+  if (emailValue === "" || passwordValue === "") {
+    alert("All form fields must be filled in");
+  }
+  const objectUserInfo = {
+    email: emailValue,
+    password: passwordValue,
+  };
+  console.log(objectUserInfo);
+  form.reset();
+});
