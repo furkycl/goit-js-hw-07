227ee58a0ee00360bc38b3694a3d5629a83ab893
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-05-05 04:34:25 +0300

    add task 1 css

--- js/task-1.js
+++ js/task-1.js
@@ -9,3 +9,29 @@ categories.forEach((category) => {
   }
   console.log(`Total Content: ${content.length}`);
 });
+
+const listHeaders = document.querySelectorAll("#categories .item h2");
+listHeaders.forEach((h2) => {
+  h2.style.fontFamily = "Montserrat";
+  h2.style.fontWeight = "600";
+  h2.style.fontSize = "24px";
+  h2.style.lineHeight = "32px";
+  h2.style.marginLeft = "16px";
+});
+
+const listItems = document.querySelectorAll("#categories li");
+listItems.forEach((li) => {
+  li.style.listStyle = "none";
+  li.style.borderRadius = "5px";
+  li.style.padding = "5px";
+  li.style.marginBottom = "13px";
+  li.style.backgroundColor = "#f6f6fe";
+});
+const contentItems = document.querySelectorAll("#categories .item ul li");
+contentItems.forEach((li) => {
+  li.style.border = "1px solid #000000";
+  li.style.marginBottom = "8px";
+  li.style.fontFamily = "Montserrat";
+  li.style.fontWeight = "400";
+  li.style.lineHeight = "24px";
+});
