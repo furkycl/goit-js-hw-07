785e7593729d19b97f79b93a763b2f4f3df31e7a
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-05-05 04:48:51 +0300

    add task 3 css

--- js/task-3.js
+++ js/task-3.js
@@ -4,3 +4,9 @@ nameInput.addEventListener("input", (event) => {
   const trimmedName = event.currentTarget.value.trim();
   nameOutput.textContent = trimmedName !== "" ? trimmedName : "Anonymous";
 });
+const header = document.querySelector("h1");
+nameInput.style.width = "360px";
+nameInput.style.height = "40px";
+header.style.fontFamily = "Montserrat";
+header.style.fontSize = "24px";
+header.style.lineHeight = "32px";
