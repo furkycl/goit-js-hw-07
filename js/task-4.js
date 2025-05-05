31bf2ca9e6fae5c186554da79839d19482b69657
Furkan YÜCEL <81173127+furkycl@users.noreply.github.com>
2025-05-05 05:18:34 +0300

    add task 4 css half done

--- js/task-4.js
+++ js/task-4.js
@@ -14,3 +14,42 @@ form.addEventListener("submit", function (event) {
   console.log(objectUserInfo);
   form.reset();
 });
+const loginForm = document.querySelector(".login-form");
+loginForm.style.display = "flex";
+loginForm.style.flexDirection = "column";
+const labels = loginForm.querySelectorAll("label");
+labels.forEach((label) => {
+  label.style.fontFamily = "Montserrat";
+  label.style.fontWeight = "400";
+  label.style.fontSize = "16px";
+  label.style.lineHeight = "24px";
+  label.style.display = "flex";
+  label.style.flexDirection = "column";
+  label.style.marginBottom = "8px";
+});
+const inputs = loginForm.querySelectorAll("input");
+labels.forEach((input) => {
+  input.style.width = "360px";
+  input.style.height = "40px";
+  input.style.marginBottom = "16px";
+});
+
+const loginButton = loginForm.querySelector("button");
+loginButton.style.backgroundColor = "#4e75ff";
+loginButton.style.width = "86px";
+loginButton.style.height = "40px";
+loginButton.style.borderRadius = "8px";
+loginButton.style.border = "none";
+loginButton.style.color = "#ffff";
+loginButton.style.cursor = "pointer";
+
+const styleInputsHover = document.createElement("styleInputsHover");
+styleInputsHover.textContent = `
+.style-inputs-hover:hover {
+  border : 3px solid #000000;
+}
+`;
+document.head.appendChild(style);
+inputs.forEach((input) => {
+  input.classList.add("style-inputs-hover:hover");
+});
